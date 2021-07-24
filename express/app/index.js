e82bcb90e34c2dc012938b69d36676e0e984ecc1
Yegor Bitensky <yegorbitensky@gmail.com>
2021-07-24 17:38:28 +0300

    Add cahche reset logic

--- express/app/index.js
+++ express/app/index.js
@@ -1,10 +1,23 @@
 const express = require('express');
+const fs = require('fs-extra');
+
 const app = express();
 const port = 8080;
 
 
 app.get('/', (req, res) => {
-  res.send('Hello World!2');
+  res.send('Hello World!1');
+});
+
+
+app.post('/reset_cache/', (req, res) => {
+  fs.emptyDir('/cache/', err => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.send('Done!');
+    }
+  })
 });
 
 
